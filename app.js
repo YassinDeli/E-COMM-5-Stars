07d410aa31574ced524de7c07accb7bcce970467
YassinDeli <yassindeli24@gmail.com>
2024-06-12 17:32:19 +0100

    New Js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-hello 
+const CartBtn=document.querySelector(".cart-btn");
+const closeCartBtn=document.querySelector(".close-cart ");
+const clearCartBtn=document.querySelector(".clear-cart");
+
+const CartDOM=document.querySelector(".cart");
+const CartOverlay=document.querySelector(".cart-overlay");
+const Cartitems=document.querySelector(".cart-items");
+const CartOverTotal=document.querySelector(".cart-Total");
+const CartOverContent=document.querySelector(".cart-content");
+const productsDOM=document.querySelector(".ptoduct-center");
+
+//cart
+let cart =[]
+
+//getting the products
+class products{
+
+}
+
+//dispay products
+loclal Storage{
+
+}
+document.addEventListener("DOMContentLoaded",()=>{
+    const ui=new UI();
+    const products = new products();
+});
